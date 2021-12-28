bbf546c84582f0913ce87824823be4cd211e699e
Hemant Chaubey <codewithhemant@gmail.com>
2021-12-28 21:41:06 +0530

    added carousel

---
+++ docs/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	"use strict";
+	
+    $(".carousel-sliders").owlCarousel({
+		animateOut: 'fadeOut',
+    	animateIn: 'flipInX',
+	    loop: true,
+	    autoplay: true,
+        autoplayTimeout: 5000,
+	    nav: true,
+	    dots: false,
+	    navContainer: '.banner .custom-nav',
+	    items: 1,
+	});
+  
+});
+
+window.onscroll = function() {toggleMenu()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop + navbar.offsetHeight;
+
+function toggleMenu() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("fixed")
+  } else {
+    navbar.classList.remove("fixed");
+  }
+}
