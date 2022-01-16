309ef62ce700670ebb297fc2e672275d0d7c818c
Hemant Chaubey <codewithhemant@gmail.com>
2022-01-16 14:44:06 +0530

    optimized the files and dirs

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,28 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  "use strict";
 
-	"use strict";
-	
-    $(".carousel-sliders").owlCarousel({
-		animateOut: 'fadeOut',
-    	animateIn: 'flipInX',
-	    loop: true,
-	    autoplay: true,
-        autoplayTimeout: 5000,
-	    nav: true,
-	    dots: false,
-	    navContainer: '.banner .custom-nav',
-	    items: 1,
-	});
+  $(".carousel-sliders").owlCarousel({
+    animateOut: "fadeOut",
+    animateIn: "flipInX",
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    nav: true,
+    dots: false,
+    navContainer: ".banner .custom-nav",
+    items: 1,
+  });
 
-	$(".carousel-testimonials").owlCarousel({
-		loop:true,
-		margin:10,
-		nav:true,
-		items: 1
-	});
+  $(".carousel-testimonials").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 1,
+  });
 });
 
-window.onscroll = function() {toggleMenu()};
+window.onscroll = function () {
+  toggleMenu();
+};
 
 var navbar = document.getElementById("navbar");
 
@@ -30,7 +31,7 @@ var sticky = navbar.offsetTop + navbar.offsetHeight;
 
 function toggleMenu() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("fixed")
+    navbar.classList.add("fixed");
   } else {
     navbar.classList.remove("fixed");
   }
